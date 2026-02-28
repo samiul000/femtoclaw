e56e3cff2523a444b51b7dbcc5669b88c1ba6b6b
Al Mahmud Samiul <123773748+samiul000@users.noreply.github.com>
2026-02-28 23:31:24 +0600

    Delete firmware/esp32c3 directory

--- firmware/esp32c3/FrameworkArduino/libb64/cencode.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32c3/FrameworkArduino/libb64/cencode.c.o: \
- C:/Users/ACER/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.c \
- C:/Users/ACER/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.h
